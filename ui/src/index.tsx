481175c24489c555c19cf5f87e7ea445a35400ad
Javier Solana Huertas <javier.solana@cabify.com>
2024-01-17 09:20:58 +0100

    Application extension

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,24 +1,10 @@
 ((window: any) => {
   const component = () => {
-    return React.createElement("div", {}, "Hello World");
+    return React.createElement("div", {}, "Proposed manifest diff(s)");
   };
-  window?.extensionsAPI?.registerResourceExtension(
-   component,
-   "argoproj.io/Application",
-   "argoproj.io/Application",
-   "Hello World",
-   { icon: "fa-solid fa-not-equal" }
- );
- window?.extensionsAPI?.registerResourceExtension(
-  component,
-  "*",
-  "*",
-  "Nice extension"
-);
-window?.extensionsAPI?.registerResourceExtension(component, '', 'Pod', 'Metrics1', { icon: "fa fa-chart-area" });
-window?.extensionsAPI?.registerResourceExtension(component, '*', 'Deployment', 'Metrics2', { icon: "fa fa-chart-area" });
-window?.extensionsAPI?.registerResourceExtension(component, '*', 'Application', 'Metrics3', { icon: "fa fa-chart-area" });
-window?.extensionsAPI?.registerResourceExtension(component, 'Application', 'Application', 'Metrics4', { icon: "fa fa-chart-area" });
+  
+window?.extensionsAPI?.registerResourceExtension(component, '*', 'Application', 'Proposed diff', { icon: "fa fa fa-file-medical" });
+
 
 
 const flyoutComponent = (props: {
@@ -30,7 +16,7 @@ return React.createElement(
     style: { padding: "10px" },
     onClick: () => props.openFlyout()
   },
-  "Hello World"
+  "Proposed manifest diff(s)"
 );
 };
 const flyout = () => {
